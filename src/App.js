e113e6b24207d41663cc619338ee767adae6f3b9
leostricak22 <stricak.leo@gmail.com>
2024-09-03 21:28:44 +0200

    feat: Add timer challenges and modal dialog

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import Player from "./components/Player";
+import TimerChallenge from "./components/TimerChallenge";
 
 function App() {
   return (
       <>
-        <Player />
+          <Player />
+          <div className="challenges">
+              <TimerChallenge title="Too easy" targetTime={1} />
+              <TimerChallenge title="Easy" targetTime={5} />
+              <TimerChallenge title="Medium" targetTime={10} />
+              <TimerChallenge title="Hard" targetTime={15} />
+          </div>
       </>
   );
 }
